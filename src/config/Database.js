c9f0ddaf3a143b206864724142e9e6ad368e28ee
nnanhtudev <nnanhtu.dev@gmail.com>
2023-10-30 09:51:12 +0700

    connection pool pattern

---
+++ src/config/Database.js
@@ -0,0 +1,23 @@
+// get the client
+const mysql = require("mysql2");
+// config database
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+//Connection pool pattern
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-// get the client
-const mysql = require("mysql2");
+const connection = require("./config/Database");
 
 const app = express();
 const hostname = process.env.HOST_NAME;
@@ -13,20 +12,10 @@ const port = process.env.PORT || 3000;
 configViewEngine(app);
 
 app.use("/", webRoutes);
-//test connecting to server
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
 // simple query
 connection.query("SELECT * FROM `Users`", function (err, results, fields) {
   console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
+  // console.log(fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
